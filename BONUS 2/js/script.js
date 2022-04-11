5c6d45e9c5425c660edb59fa849cb7eedd0da14d
SimoneCorona <corona.simone93@gmail.com>
2022-04-11 16:46:17 +0200

    Bonus 3 added

--- BONUS 2/js/script.js
+++ BONUS 2/js/script.js
@@ -5,7 +5,7 @@
 const rowWrapper = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
-    let _number
+    let _number; // qui vale undefined
     if (i % 15 === 0) {
         _number = "FizzBuzz";
     } else if (i % 3 === 0) {
@@ -13,21 +13,12 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 === 0) {
         _number = "Buzz";
     } else {
-        _number = parseInt(i);
+        _number = i;
     }
-    console.log(i, _number);
+    console.log(i, _number); // sicuramente non è più undefined
     // BONUS 1
     // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
     // BONUS 2
     // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. 
-    let fizzbuzz
-    if (_number = "FizzBuzz") {
-        fizzbuzz = "fizzBuzz";
-    } else if (_number = "Fizz") {
-        fizzbuzz = "fizz";
-    } else if (_number = "Buzz") {
-        fizzbuzz = "buzz";
-    }
-    console.log(fizzbuzz);
+    rowWrapper.innerHTML +=  `<div class="box ${_number}">${_number}</div>`;
 }
-rowWrapper.innerHTML +=  `<div class="box $(fizzbuzz)">${_number}</div>`;
